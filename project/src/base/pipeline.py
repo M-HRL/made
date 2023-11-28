244e02c51331d3fb97f6381f66335a8a45fd87de
M-HRL <haertl.marco@web.de>
2023-11-28 20:51:48 +0100

    make block list private

--- project/src/base/pipeline.py
+++ project/src/base/pipeline.py
@@ -3,13 +3,13 @@ from src.base.block import Block
 
 class Pipeline(Block):
     def __init__(self):
-        self.blocks: list[Block] = []
+        self._blocks: list[Block] = []
 
     def register(self, block: Block):
-        self.blocks.append(block)
+        self._blocks.append(block)
         return self
 
     def invoke(self, *args) -> tuple:
-        for block in self.blocks:
+        for block in self._blocks:
             args = block.invoke(*args)
         return args
